d60f60772c5f15cd034d85e4d891f9baa2b84254
aihdde <58286658+aihdde@users.noreply.github.com>
2024-06-01 03:03:12 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -111,6 +111,7 @@ sha256sums
 version.buildinfo
 ipk.tar.gz
 profiles.json
+openwrt-x86-64-generic-rootfs.tar.gz
 openwrt-x86-64-generic-ext4-combined.img.gz
 openwrt-x86-64-generic-ext4-combined-efi.img.gz
 openwrt-x86-64-generic-ext4-rootfs.img.gz
